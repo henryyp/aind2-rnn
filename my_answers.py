4229cd7b66e7341bb16a2382b22fc350b973f291
henryyp <henry@withreason.co.uk>
2017-12-18 09:47:59 +0800

    update latest progress

--- my_answers.py
+++ my_answers.py
@@ -6,14 +6,14 @@ from keras.layers import LSTM
 import keras
 
 
-# TODO: fill out the function below that transforms the input series 
+# TODO: fill out the function below that transforms the input series
 # and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_series(series, window_size):
     # containers for input/output pairs
-    X = []
-    y = []
+    X = [series[i:i+window_size] for i in range(0, len(series)-window_size) ]
+    y = series[window_size:]
 
-    # reshape each 
+    # reshape each
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
     y = np.asarray(y)
@@ -23,7 +23,10 @@ def window_transform_series(series, window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
+    model = Sequential()
+    model.add(LSTM(128, input_shape = (window_size,1)))
+
+    return model
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
@@ -40,7 +43,7 @@ def window_transform_text(text, window_size, step_size):
 
     return inputs,outputs
 
-# TODO build the required RNN model: 
-# a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
+# TODO build the required RNN model:
+# a single LSTM hidden layer with softmax activation, categorical_crossentropy loss
 def build_part2_RNN(window_size, num_chars):
     pass
